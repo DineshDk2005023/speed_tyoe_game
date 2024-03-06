cae25e316f927a74c5c9a6f791407ff0c501de4e
DineshDk2005023 <103164376+DineshDk2005023@users.noreply.github.com>
2024-03-07 00:16:51 +0530

    Speed type game

---
+++ index.js
@@ -0,0 +1,12 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+ 
+  const firebaseConfig = {
+    apiKey: "AIzaSyDbqd5TVGKDvvd37SksN-bfES9xK7WxgEM",
+    authDomain: "login-auth-cebf1.firebaseapp.com",
+    projectId: "login-auth-cebf1",
+    storageBucket: "login-auth-cebf1.appspot.com",
+    messagingSenderId: "692074658173",
+    appId: "1:692074658173:web:a56f2809caaa5eafe4f414"
+  };
+
+  const app = initializeApp(firebaseConfig);
